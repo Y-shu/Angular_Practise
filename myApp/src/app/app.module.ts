8d2527969d8b9d448b756e4977db03ae16b1a9cf
Y-shu <nivik12292@gmail.com>
2019-07-21 12:26:28 +0530

    Some modified content...

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Inorder to use forms-TemplateDrivenForms
-import { FormsModule } from '@angular/forms';
+// Inorder to use Reactive Forms
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,14 +10,17 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule,Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductsComponent } from './products/products.component';
 
 // Router definition
 const my_routes:Routes = [
   // We have to configure the route so then only we can use then 
   // we configure using RouterModule.forRoot()
 {'path':'home',component:HomeComponent},
+{'path':'products',component:ProductsComponent},
 // last route and wildCard
-{'path':'**',component:PageNotFoundComponent}
+{'path':'**',component:PageNotFoundComponent},
+
 
 ]
 @NgModule({
@@ -25,12 +29,15 @@ const my_routes:Routes = [
     HeaderComponent,
     HomeComponent,
     FooterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     // template driven forms
     FormsModule,
+    // reactive forms
+    ReactiveFormsModule,
     RouterModule.forRoot(my_routes)
   ],
   providers: [],
